99232116c1f05fa9958417015536780ab69c76ca
Rohan Kumar <31317534+rohanksingh@users.noreply.github.com>
2024-06-25 23:17:00 -0400

    Update multiprocess_addition.py

--- app/utils/multiprocess_addition.py
+++ app/utils/multiprocess_addition.py
@@ -1,30 +1,13 @@
-import logging 
-from multiprocessing import Pool, current_process
-from typing import List, Tuple
+from multiprocessing import Pool
+from typing import List
 
-# Set up logging
+def add_lists(sublist:List[int]) -> int:
+	return sum(sublist)
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(levelname)s] %(message)s')
-
-def add_list(pair: List[int]) -> int:
-    try:
-        logging.info(f"Process {current_process().name} adding {pair}")
-        result= sum(pair)
-        return result
-    except Exception as e:
-        logging.error(f"Error in process {current_process().name}: {e}")
-        raise
-
-def process_list_additions(list_pairs: List[List[int]], num_processes: int) -> List[int]:
-    results = []
-    with Pool(processes= num_processes) as Pool:
-        try:
-            results= pool.map(add_lists, list_pairs)
-        except Exception as e:
-            logging.error(f"Error during multiprocessing: {e}")
-            raise e 
-    return results
-
+def process_list_additions(lists: List[List[int]], num_processes: int) -> List[int]:
+	with Pool(processes= num_processes) as pool:
+		results = pool.map(add_lists , lists)
+	return results
 
 
 
