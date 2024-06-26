fb54ca190ffecedb0172de8efd111d14bbfbf747
Rohan Kumar <31317534+rohanksingh@users.noreply.github.com>
2024-06-25 22:51:50 -0400

    addition_controller.py

--- app/controllers/addition_controller.py
+++ app/controllers/addition_controller.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timezone
-from app.models.addition import AdditionRequest, AdditionResponse
+from app.models.addition_request import AdditionRequest
+from app.models.addition_response import AdditionResponse
 from app.utils.multiprocess_addition import process_list_additions
+from app.config import settings
 
 def create_addition_logic(addition_request: AdditionRequest) -> AdditionResponse:
     started_at = datetime.now(timezone.utc)
 
     try:
-        response_list = process_list_additions(addition_request.payload, num_processes=4)
-        result = [sum(sublist) for sublist in response_list]
+        result = process_list_additions(addition_request.payload, num_processes=settings.NUM_PROCESSES)
     except ValueError as e:
         raise e
 
